b9286702a7654ce1ef1e4674d408c0a75bc3357e
bruzen <k.w.robinson@gmail.com>
2023-06-12 23:39:45 -0400

    Test Mesa with Conway's Game of Life

--- model.py
+++ model.py
@@ -1,47 +1,51 @@
 import random
 import numpy as np
-import matplotlib.pyplot as plt
+import pandas as pd
 import streamlit as st
+import altair as alt
+import matplotlib.pyplot as plt
+from mesa import Agent
+from mesa import Model
+from mesa.space import SingleGrid
+from mesa.time import SimultaneousActivation
+from agent import Cell
 
+class ConwaysGameOfLife(Model):
+    """
+    Represents the 2-dimensional array of cells in Conway's
+    Game of Life.
+    """
 
-class Agent:
-    def __init__(self, x, y, num_states):
-        self.x = x
-        self.y = y
-        self.state = random.randint(0, num_states - 1)
+    def __init__(self, width=50, height=50):
+        """
+        Create a new playing area of (width, height) cells.
+        """
 
-    def step(self, model):
-        self.state += 1
-        self.state %= model.num_states
+        # Set up the grid and schedule
+        self.grid = SingleGrid(width, height, torus=True)
+        self.schedule = SimultaneousActivation(self)
 
+        # Place a cell at each location, with some initialized to ALIVE and some to DEAD
+        for x in range(width):
+            for y in range(height):
+                cell = Cell((x, y), self)
+                if random.random() < 0.1:
+                    cell.state = cell.ALIVE
+                self.grid.place_agent(cell, (x, y))
+                self.schedule.add(cell)
 
-class SimpleModel:
-    def __init__(self, N, width, height, num_states):
-        self.num_agents = N
-        self.grid_width = width
-        self.grid_height = height
-        self.num_states = num_states
-        self.grid = np.zeros((width, height), dtype=int)
-        self.schedule = []
-
-        # Create agents
-        for i in range(self.num_agents):
-            x = random.randint(0, self.grid_width - 1)
-            y = random.randint(0, self.grid_height - 1)
-            agent = Agent(x, y, self.num_states)
-            self.schedule.append(agent)
-            self.grid[x, y] = agent.state
+        self.running = True
 
     def step(self):
-        random.shuffle(self.schedule)
-        for agent in self.schedule:
-            agent.step(self)
-            self.grid[agent.x, agent.y] = agent.state
+        """
+        Have the scheduler advance each cell by one step
+        """
+        self.schedule.step()
 
 
 def run_model(num_steps):
-    # Create a simple model
-    model = SimpleModel(N=100, width=10, height=10, num_states=5)
+    # Create a Game of Life model
+    model = ConwaysGameOfLife(width=50, height=50)
 
     # Create data for visualization
     agent_data = []
@@ -52,25 +56,41 @@ def run_model(num_steps):
         model.step()
 
         # Collect agent state data
-        agent_state_counts = np.zeros(model.num_states, dtype=int)
-        for agent in model.schedule:
-            agent_state_counts[agent.state] += 1
+        agent_state_counts = np.zeros(2, dtype=int)
+        for cell in model.schedule.agents:
+            agent_state_counts[cell.state] += 1
         agent_data.append(agent_state_counts.copy())
 
-    # Convert data to numpy array and transpose for plotting
-    agent_data = np.array(agent_data).T
+    # Convert data to Pandas DataFrame for Altair visualization
+    df = pd.DataFrame(agent_data, columns=['DEAD', 'ALIVE'])
+    df['Step'] = np.arange(num_steps)
 
-    # Plot grid and agent state evolution
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
-    ax1.imshow(model.grid, cmap='viridis')
-    ax1.set_title("Agent State Grid")
-    ax1.axis('off')
-    ax2.plot(agent_data)
-    ax2.set_title("Agent State Evolution")
-    ax2.set_xlabel("Step")
-    ax2.set_ylabel("Count")
+    # Create the Altair time series plot
+    chart = alt.Chart(df).transform_fold(
+        ['DEAD', 'ALIVE'],
+        as_=['State', 'Count']
+    ).mark_line().encode(
+        x='Step:Q',
+        y='Count:Q',
+        color='State:N'
+    ).properties(
+        width=400,
+        height=300
+    )
 
     # Display the plots using Streamlit
+    st.altair_chart(chart, use_container_width=True)
+
+    # Display the agent state grid
+    st.subheader("Agent State Grid")
+
+    grid_data = np.zeros((model.grid.width, model.grid.height), dtype=int)
+    for cell in model.schedule.agents:
+        grid_data[cell.pos[0], cell.pos[1]] = cell.state
+
+    fig, ax = plt.subplots()
+    ax.imshow(grid_data, cmap='viridis')
+    ax.axis('off')
     st.pyplot(fig)
 
 
